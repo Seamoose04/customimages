1c25b8ed0a73455d5bcfbc3d46667cc68cab13fd
Seamus Johnson <72893263+Seamoose04@users.noreply.github.com>
2024-04-19 18:28:57 -0400

    Created

---
+++ CustomImages.ts
@@ -0,0 +1,11 @@
+/**
+* Custom blocks
+*/
+
+//% color=#ace3bb icon="📷"
+namespace ImageConverter {
+    //% block
+    export function image(picture: String) {
+        image_from_code(picture);
+    }
+}// Add your code here
--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+function image_from_code(picture: String): Image {
+    let width: number = parseInt(picture.slice(0, 3));
+    let height: number = parseInt(picture.substr(0, 3));
+    picture = picture.slice(6);
+    let output_image: Image = image.create(width, height);
+    for (let i = 0; i < width; i++) {
+        for (let j = 0; j < height; j++) {
+            output_image.setPixel(i, j, parseInt(picture.slice(0, 2)));
+            picture.slice(1);
+        }
+    }
+    return output_image;
+}
