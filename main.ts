e21ca099207163b19095e843b82e983576bf6a7f
Seamus Johnson <72893263+Seamoose04@users.noreply.github.com>
2024-04-19 18:33:05 -0400

    Almost?

--- main.ts
+++ main.ts
@@ -3,9 +3,9 @@ function image_from_code(picture: String): Image {
     let height: number = parseInt(picture.substr(0, 3));
     picture = picture.slice(6);
     let output_image: Image = image.create(width, height);
-    for (let i = 0; i < width; i++) {
-        for (let j = 0; j < height; j++) {
-            output_image.setPixel(i, j, parseInt(picture.slice(0, 2)));
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            output_image.setPixel(j, i, parseInt(picture.slice(0, 2)));
             picture = picture.slice(1);
         }
     }
