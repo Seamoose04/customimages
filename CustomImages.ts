303a4d1ef4de93219aa72a741153440b1f34a279
Seamus Johnson <72893263+Seamoose04@users.noreply.github.com>
2024-04-19 18:29:32 -0400

    Fixed return

--- CustomImages.ts
+++ CustomImages.ts
@@ -5,7 +5,7 @@
 //% color=#ace3bb icon="📷"
 namespace ImageConverter {
     //% block
-    export function image(picture: String) {
-        image_from_code(picture);
+    export function image(picture: String): Image {
+        return image_from_code(picture);
     }
 }// Add your code here
